fe065e36ba5db28860ec51ceb9c35772c0731768
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2019-01-02 19:54:39 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  devise_for :owners
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :owner do
+    authenticated :owner do
+      root 'home#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  
+  get "/home", to: "home#index"
+  get '/callback'
+  post "change_light_state", to: "home#change_light_state"
+  post "toggle_light", to: "home#toggle_light"
+  
+  
+end
