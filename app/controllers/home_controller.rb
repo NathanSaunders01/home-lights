06142317e71830335f7f30bf974e5d011a37f2d4
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2019-01-04 12:16:34 +0000

    Updated Callback for digest

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -48,7 +48,7 @@ class HomeController < ApplicationController
     resp = http.request(request)
     puts resp.inspect
     puts resp
-    puts resp.body
+    puts resp.headers
     # body = resp.body
     # data = JSON.parse body
     # puts data
