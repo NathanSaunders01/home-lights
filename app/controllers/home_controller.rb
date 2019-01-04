561d4a9b4978b777c355a8c517615f65cd51b38e
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2019-01-04 22:32:51 +0000

    Updated callback for digest

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -79,7 +79,7 @@ class HomeController < ApplicationController
     new_uri = URI.parse("https://api.meethue.com/oauth2/token?code=#{params[:code]}&grant_type=authorization_code")
 
     new_http = Net::HTTP.new(new_uri.host, new_uri.port)
-    new_request = Net::HTTP::Post.new(new_uri.path, initheader = {'Authorization' =>"#{digest}"})
+    new_request = Net::HTTP::Post.new(new_uri.path, initheader = {'Authorization': "#{digest}"})
     puts "test prep"
     new_http.use_ssl = true
     # new_request['Authorization'] = "Digest #{digest}"
